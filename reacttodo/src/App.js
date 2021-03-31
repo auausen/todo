ed58803e5048ce4c1b1577ad7023ce8eba898ff4
naka <ryousen.naka@microworld.co.jp>
2021-03-31 15:47:34 +0900

    end

--- reacttodo/src/App.js
+++ reacttodo/src/App.js
@@ -1,18 +1,68 @@
-import React, {useState} from "react";
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //起動時に実行
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  // useeffect
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case `completed`:
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case `umcompleted`:
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+  // ローカル保存
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      setTodos(JSON.parse(localStorage.getItem('todos')));
+    }
+  };
+
   return (
     <div className="App">
       <header>
         <h1>my todo list</h1>
       </header>
-      <Form setInputText={setInputText}/>
-      <TodoList />
+      <Form
+        todos={todos}
+        inputText={inputText}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
--- reacttodo/src/components/Form.js
+++ reacttodo/src/components/Form.js
@@ -1,18 +1,37 @@
 import React from "react";
 
-const Form = ({ setInputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
   const inputTextHandler = (e) => {
-    console.log(e.target.value);
-    serInputText(e.target.value);
+    setInputText(e.target.value);
+  };
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
   };
   return (
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" classNameName="filter-todo">
+        <select
+          onChange={statusHandler}
+          name="todos"
+          classNameName="filter-todo"
+        >
           <option value="all">All</option>
           <option value="completed">完了</option>
           <option value="uncompleted">未完了</option>
